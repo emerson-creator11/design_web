8ab7e07032147c1fd5f5ef75c5549f313b4ef347
emerson-creator11 <c.emerson@escolar.ifrn.edu.br>
2026-08-20 13:15:37 -0300

    coloquei informações no java script

--- script/app.js
+++ script/app.js
@@ -1,1 +1,15 @@
 alert("olá mundo !")
+
+var nasc = 2009;
+let nome = "Emerson";
+const vivo = true
+let altura = 1.85;
+
+if (vivo) {
+    let saudacao = "ola, " + nome + "!";
+    let mensagem = `altura: ${altura}m | idade: ${2025-nasc}` ;
+    alert (saudacao+ '\n'+mensagem);
+}
+else {
+    alert("Voce não está vivo");
+}
