2eb4cc3e45dd4ec13f677a582dec4f58a4cff62c
emerson-creator11 <c.emerson@escolar.ifrn.edu.br>
2026-09-04 14:20:42 -0300

    aprendendo sobre o continue

--- js/app4.js
+++ js/app4.js
@@ -1,5 +1,9 @@
 let vezes = Number(prompt("Digite o numero de vezes: "));
 for(let i = 1;i<=vezes; i++ ){
     alert(`contei ${i} vezes`);
+    if(i%2!=0){
+        continue;
+    }
+    alert(`${i} é par`);
 
 }
