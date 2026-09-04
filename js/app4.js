de073da7216e4589a2f62fd1fc975607eb00624f
emerson-creator11 <c.emerson@escolar.ifrn.edu.br>
2026-09-04 14:25:49 -0300

    break ele serve para que se um numero passar do valor ele para

--- js/app4.js
+++ js/app4.js
@@ -1,9 +1,14 @@
 let vezes = Number(prompt("Digite o numero de vezes: "));
 for(let i = 1;i<=vezes; i++ ){
+    if (vezes > 100){
+        alert("valor invalido, digite um numero entre 0 e 100");
+        break
+    }
     alert(`contei ${i} vezes`);
     if(i%2!=0){
         continue;
     }
     alert(`${i} é par`);
+    
 
 }
